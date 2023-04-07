ce9035d1dadcdb748312637e1e18382a2fca44b2
Maaaksimus <plowecz@mail.ru>
2023-04-07 13:04:46 +0300

    start

---
+++ baselib.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <vector>
+
+#define tau 1e-2
+
+using namespace std;
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "baselib.h"
+
+int main() {
+    int n;
+    double h;
+    cin >> n;
+    h  = 1. / (n - 1);
+}
